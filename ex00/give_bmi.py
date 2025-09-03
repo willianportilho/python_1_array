69b504fc4c6da8ee3d6e02127e7c66480cdfa137
Willian <willianportilho01@gmail.com>
2025-09-02 20:32:07 -0400

    feat: adds ex00/ folder and initial files give_bmi.py and tester.py

---
+++ ex00/give_bmi.py
@@ -0,0 +1,12 @@
+def give_bmi(
+        height: list[int | float],
+        weight: list[int | float]
+        ) -> list[int | float]:
+    print("1")
+
+
+def apply_limit(
+        bmi: list[int | float],
+        limit: int
+        ) -> list[bool]:
+    print("2")
