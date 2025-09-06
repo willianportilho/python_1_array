adeaf601118757035bb8ad0deda39d1b2aa90285
Willian <willianportilho01@gmail.com>
2025-09-05 20:09:18 -0400

    feat: adds some aditional tests; fix: some small bugs and wrong messages

--- ex02/load_image.py
+++ ex02/load_image.py
@@ -4,7 +4,13 @@ import numpy as np
 
 def ft_load(path: str) -> np.ndarray:
     try:
-        img = Image.open(path)
+        if not path.lower().endswith((".jpg", ".jpeg")):
+            raise TypeError("The file is not a JPEG or JPG extension")
+
+        try:
+            img = Image.open(path)
+        except Exception:
+            raise ValueError(f"Cannot identify or corrupted file '{path}'")
 
         if img.format not in ("JPEG", "JPG"):
             raise ValueError("The file is not a JPEG or JPG image")
@@ -18,15 +24,11 @@ def ft_load(path: str) -> np.ndarray:
 
         return img_array
 
-    except (FileNotFoundError, IOError, ValueError) as e:
+    except (FileNotFoundError, IOError, ValueError, TypeError) as e:
         print(f"Error: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
     return None
-
-
-def ft_load_tests():
-    pass
 
 
 def main():
@@ -40,13 +42,29 @@ Returns:
     None
     """
     try:
-        pass
-    except AssertionError as ae:
-        print(f"❌ {ft_load_tests.__name__} assertion failed: {ae}")
+        tests = [
+            ("landscape.jpg", True),
+            ("animal.jpeg", True),
+            ("non_existent_file.jpg", False),
+            ("non_image_file.txt", False),
+            ("empty_image.jpeg", False),
+            ("corrupted_image.jpg", False),
+            ("not_jpeg_or_jpg_image.png", False)
+        ]
+
+        for path, expected in tests:
+            print(f"\n=== TESTING: {path}")
+            result = ft_load(path)
+
+            is_loaded = result is not None
+
+            if is_loaded == expected:
+                print(f"✅ Expected behavior!: path: {path}\n")
+            else:
+                print(f"❌ Unexpected behavior!: path: {path}\n")
+
     except Exception as e:
-        print(f"❌ unexpected exception: {e}")
-    else:
-        print(f"\n✅ All tests in {ft_load_tests.__name__} passed.")
+        print(f"unexpected exception: {e}")
 
 
 if __name__ == "__main__":
